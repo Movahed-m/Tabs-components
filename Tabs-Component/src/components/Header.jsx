c6f5d48d18895418af816000db5f0090d5033c69
Mohammad Movahed <mohammad03@yahoo.com>
2024-08-26 08:13:00 +0330

    make header component and change title of main tab in index.html

---
+++ Tabs-Component/src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header>
+      <div className="header-container">
+        <h1 className="header-title">Tabs Component with React</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
